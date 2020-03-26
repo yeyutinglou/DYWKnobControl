74306e7178aaff5466edbf96b3e5ea991d23b16c
516926914 <516926914@qq.com>
2020-03-26 17:52:54 +0800

    Initial Commit

---
+++ DYWKnobControl/DYWKnobControl.h
@@ -0,0 +1,19 @@
+//
+//  DYWKnobControl.h
+//  DYWKnobControl
+//
+//  Created by jyd on 2020/3/26.
+//  Copyright © 2020 jyd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DYWKnobControl.
+FOUNDATION_EXPORT double DYWKnobControlVersionNumber;
+
+//! Project version string for DYWKnobControl.
+FOUNDATION_EXPORT const unsigned char DYWKnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DYWKnobControl/PublicHeader.h>
+
+
